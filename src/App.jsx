4ab350fabe5060358b9b0f27fd85bc393d9bbfea
Guide <thammatorn.sud@gmail.com>
2024-10-14 15:31:36 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Form from "./components/Form";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar ></Navbar>
+      <Form ></Form>
+
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className="navbar justify-center bg-base-200">
+  <p className="  text-3xl">User menagement</p>
+</div>
+  )
+}
+export default Navbar;
